1e88093757c90e745d9940d8d895168e4fa67df6
Jon Penn <PennPD@sbcglobal.net>
2012-11-02 10:54:43 +0400

    migration to git

---
+++ demos/led-pwm-demo.c
@@ -0,0 +1,37 @@
+/*
+ * led-pwm-demo.c
+ * Copyright (C) 2012  Jon Penn
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "robotlib.h"
+
+void greenFlag(){
+	int i;
+	setPinMode(10, OUTPUT);
+	setPinMode(16, OUTPUT);
+	for(;;){
+		for(i=0; i<100; i+=20){
+			analogWrite(10, i);
+			analogWrite(16, 100-i);
+			wait(1);
+		}
+		for(;i>=0; i-=20){
+			analogWrite(10, i);
+			analogWrite(16, 100-i);
+			wait(1);
+		}
+	}
+}
